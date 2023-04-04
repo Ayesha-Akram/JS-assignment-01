df47d6c96fe0d504ef7cb396072f2aef85e9c57c
Ayesha Akram <128508163+Ayesha-Akram@users.noreply.github.com>
2023-04-04 03:42:17 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// Q1)
+
+// alert("Hello World !");
+
+// Q2)
+
+// alert("JavaScript Alert \n\n Error! Please enter a valid password.")
+
+// Q3)
+
+// alert("JavaScript Alert \n\n Welcome to JS Land...\n Happy Coding!");
+
+// Q4)
+
+// alert("JavaScript Alert \n\n Welcome to JS Land...");
+// alert("JvaScript Alert \n\n Happy Coding! \n Prevent this page from creating additional dialogs");
+
+// Q5)
+
+alert("JavaScript Alert \n\n Hello..! I can run JS through my web browser's console");
